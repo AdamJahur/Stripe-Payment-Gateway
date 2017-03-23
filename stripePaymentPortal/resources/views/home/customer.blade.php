fcd2ebc35dbb31b18bf38b35458450a89d320f85
Adam Jahur <adamnjahur@gmail.com>
2017-03-22 20:22:14 -0400

    home views now dry

--- stripePaymentPortal/resources/views/home/customer.blade.php
+++ stripePaymentPortal/resources/views/home/customer.blade.php
@@ -1,60 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <title>Customer Home Page</title>
+@extends('homeLayout')
 
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+@section('title', 'customerHome')
+@section('sectionName', 'Profile')
 
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-    <script src="https://js.stripe.com/v3/"></script>
-
-</head>
-<style>
-    table, th, td {
-        border: 1px solid black;
-        border-collapse: collapse;
-    }
-    th, td {
-        padding: 5px;
-        text-align: left;
-    }
-</style>
-<body>
-
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
-        <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Customer</a>
-        </div>
-
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-                <li><a href="#">Home</a></li>
-                <li><a href="./customerProfile">Profile</a></li>
-                <li><a href="./paymentHistoryCustomer">Payment History</a></li>
-                <li><a href="#">Logout</a></li>
-            </ul>
-            <div class="nav navbar-nav navbar-right">
-                <img src="" alt="Img">
-            </div>
-        </div><!-- /.navbar-collapse -->
-    </div><!-- /.container-fluid -->
-</nav>
+@section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-4">
@@ -78,7 +27,14 @@
 <div class="container" style="margin-top: 30px;">
     <div class="row">
         <div class="col-md-12">
-            <table style="width:100%">
+            <table style="width:100%; table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+    th, td {
+        padding: 5px;
+        text-align: left;
+    }">
                 <tr>
                     <th>Money Owed</th>
                     <th>Due Date:</th>
@@ -107,6 +63,4 @@
     </div>
 </div>
 
-
-</body>
-</html>
+@stop
